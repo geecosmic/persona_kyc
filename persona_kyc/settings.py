63bc7f8a6e1744a824277714f8ba25b8ebd614b3
George <eyogeorge23@gmail.com>
2026-08-14 17:10:55 +0100

    Update settings.py

--- persona_kyc/settings.py
+++ persona_kyc/settings.py
@@ -25,7 +25,7 @@ load_dotenv(os.path.join(BASE_DIR, ".env"))
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*y0nd_*7g+2xb-_zyu*nh@^^4jjvt81ke2zl$#(evz4qp+fsv8'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
